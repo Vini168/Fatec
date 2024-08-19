a7d3db5c517411132fd2dbb168a3e9bf740de492
Vini168 <96179019+Vini168@users.noreply.github.com>
2024-08-19 18:47:12 -0300

    Add files via upload

---
+++ PROG_WEB/Hello/src/main/java/br/edu/fateccotia/isw029/service/HelloService.java
@@ -0,0 +1,22 @@
+package br.edu.fateccotia.isw029.service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.fateccotia.isw029.model.Hello;
+
+public class HelloService {
+	private static final List<Hello> lista = new ArrayList<Hello>();
+	
+	public void create(String line) {
+		Hello hello = new Hello();
+		hello.setName(line);
+		hello.setDate(LocalDateTime.now());
+		lista.add(hello);
+	}
+	
+	public List<Hello> findAll(){
+		return lista;
+	}
+}
