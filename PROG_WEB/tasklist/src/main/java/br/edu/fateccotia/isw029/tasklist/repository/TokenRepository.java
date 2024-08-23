664b5e429d8ef8ce4dc2111fac56ca5c12b5afa7
Vinícius Ferreira <96179019+Vini168@users.noreply.github.com>
2024-08-22 22:13:17 -0300

    Add files via upload

---
+++ PROG_WEB/tasklist/src/main/java/br/edu/fateccotia/isw029/tasklist/repository/TokenRepository.java
@@ -0,0 +1,11 @@
+package br.edu.fateccotia.isw029.tasklist.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.fateccotia.isw029.tasklist.model.Token;
+
+@Repository
+public interface TokenRepository extends CrudRepository<Token, Integer>{
+
+}
